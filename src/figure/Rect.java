4eaaf73c3289bdbc4422b356a15136c85ca0b40d
asahi <lovingcat222@gmail.com>
2015-11-11 23:14:14 +0900

    間違えたので修正

--- src/figure/Rect.java
+++ src/figure/Rect.java
@@ -14,7 +14,8 @@ public class Rect extends Figure{
 	public void draw(Transform t){
 		Graphics2D g2d = (Graphics2D)component.getGraphics();
 		AffineTransform ord = g2d.getTransform();
-		
+		g2d.setTransform(t.getAffineTransform());
+
 		g2d.drawRect(- width / 2, - length, width, length);
 		
 		g2d.setTransform(ord);
